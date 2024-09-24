752e4947b878b6521a43cee0f4ee97c710f9fdff
STVOY66 <ethan.pauly@gmail.com>
2024-09-24 11:24:05 -0400

    Draft header for player class.

--- src/player.h
+++ src/player.h
@@ -10,13 +10,23 @@ class Player2D {
         Player2D(Vector2 pos, Vector2 uDir, float lSpeed, float rSpeed); // position, unit vector as direction, linear speed, rotational speed
         ~Player2D(); // destructor
 
-        Vector2 GetPos();
-        Vector2 GetDir();
-        float GetLSpeed();
-        float GetRSpeed();
-    private:
         Vector2 position; // position of player in 2d space
-        Vector2 direction; // direction player's facing as a 2d unit vector
+
+        // gets/sets for look direction, set returns an int for error-handling
+        Vector2 GetLookDir();
+        int SetLookDir();
+        // gets/sets for move direction
+        Vector2 GetMoveDir();
+        int SetMoveDir();
+        // gets/sets for linear and rotational speed
+        float GetLSpeed();
+        int SetLSpeed();
+        float GetRSpeed();
+        int SetRSpeed();
+    private:
         float lSpeed; // linear speed
         float rSpeed; // rotational speed
+        Vector2 ldir; // direction player's facing as a 2d unit vector
+        Vector2 mdir; // direction player's moving as a 2d unit vector (Idea to deal with collisions)
+
 };
